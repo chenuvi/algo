3d604a9935d8c6a7cd74884070881b091ce803ba
chenuvi <chenuvi@outlook.com>
2021-11-26 16:50:24 +0800

    Feat: 选择排序

--- src/ts/data-structure/11-sorts/SampleSort.ts
+++ src/ts/data-structure/11-sorts/SampleSort.ts
@@ -12,12 +12,14 @@
 enum SortType {
   BubbleSort,
   InsertSort,
+  SelectSort
 }
 
 interface SortAlgo {
   sort(array: number[]): void
 }
 
+// 冒泡排序
 class BubbleSort implements SortAlgo {
   sort(array: number[]) {
     let flag = false;
@@ -37,6 +39,7 @@ class BubbleSort implements SortAlgo {
   }
 }
 
+// 插入排序
 class InsertSort implements SortAlgo {
   sort(array: number[]) {
     for (let i = 1; i < array.length; i++) {
@@ -55,6 +58,26 @@ class InsertSort implements SortAlgo {
   }
 }
 
+// 选择排序 https://zhuanlan.zhihu.com/p/123048793
+// 选择排序的思想其实和冒泡排序有点类似，都是在一次排序后把最小的元素放到最前面，或者将最大值放在最后面。但是过程不同，冒泡排序是通过相邻的比较和交换。而选择排序是通过对整体的选择，每一趟从前往后查找出无序区最小值，将最小值交换至无序区最前面的位置。
+class SelectSort implements SortAlgo {
+  sort(array: number[]) {
+    if (array.length <= 1) return;
+    // 需要注意这里的边界, 因为需要在内层进行 i+1后的循环，所以外层需要 数组长度-1
+    for (let i = 0; i < array.length; i++) {
+      let minIndex = i;
+      for (let j = i + 1; j < array.length; j++) {
+        if (array[j] < array[minIndex]) {
+          minIndex = j;
+        }
+      }
+      const temp = array[i];
+      array[i] = array[minIndex];
+      array[minIndex] = temp;
+    }
+  }
+}
+
 class SortFactory {
   static getSortAlgo(type: SortType): SortAlgo {
     switch (type) {
@@ -62,6 +85,8 @@ class SortFactory {
         return new BubbleSort();
       case SortType.InsertSort:
         return new InsertSort();
+      case SortType.SelectSort:
+        return new SelectSort();
       default:
         throw new Error('unknown sort algorithm type');
     }
@@ -69,7 +94,7 @@ class SortFactory {
 }
 
 
-const insertSort = SortFactory.getSortAlgo(SortType.InsertSort)
-const test1 = [1, 0, 2, 4, 8, 5, 10]
-insertSort.sort(test1)
-console.log(test1)
+const insertSort = SortFactory.getSortAlgo(SortType.InsertSort);
+const test1 = [1, 0, 2, 4, 8, 5, 10];
+insertSort.sort(test1);
+console.log(test1);
