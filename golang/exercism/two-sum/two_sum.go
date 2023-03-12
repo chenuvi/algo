863f540f219b385f487df722e17e92a975855ace
chenuvi <chenuvi@outlook.com>
2023-03-12 21:11:54 +0800

    Feat: 两数之和

--- golang/exercism/two-sum/two_sum.go
+++ golang/exercism/two-sum/two_sum.go
@@ -1,31 +1,14 @@
 package two_sum
 
-import "fmt"
-
-func main() {
-	TwoSum([...]int{2, 7, 11, 15}, 9) // 0,1
-}
-
 // TwoSum 两数之和
-// TODO
-func TwoSum(nums []int, target int) {
+func TwoSum(nums []int, target int) []int {
 	var numsMap = make(map[int]int)
-	for index, value := range nums {
-		numsMap[value] = index
-	}
-	for index, _ := range nums {
-		temp := target - nums[index]
-		if numsMap[temp] {
-			return [2]int{index, numsMap[temp]}
+	for index, val := range nums {
+		r := target - val
+		if j, ok := numsMap[r]; ok {
+			return []int{index, j}
 		}
+		numsMap[val] = index
 	}
-	fmt.Printf("numsMap: %v", numsMap)
-	// 1. 暴力循环算法
-	//for j := 0; j < len(nums); i++ {
-	//  for i := 0; i < len(nums); i++ {
-	//    if nums[j] + nums[i] == target && j != i {
-	//      return [i, j]
-	//    }
-	//  }
-	//}ß
+	return nil
 }
--- golang/exercism/two-sum/two_sum_test.go
+++ golang/exercism/two-sum/two_sum_test.go
@@ -12,22 +12,22 @@ type charactersState struct {
 func TestTwoSum(t *testing.T) {
 	tests := []charactersState{
 		{
-			desc:          "nums = [2,7,11,15], target = 9 ",
-      nums:          [2,7,11,15],
-      target:        9,
-			expected:      [0,1],
+			desc:     "nums = [2,7,11,15], target = 9 ",
+			nums:     []int{2, 7, 11, 15},
+			target:   9,
+			expected: []int{0, 1},
 		},
 		{
-			desc:          "nums = [3,2,4], target = 6",
-			nums: [3,2,4],
-      target: 6,
-			expected:      [1,2],
+			desc:     "nums = [3,2,4], target = 6",
+			nums:     []int{3, 2, 4},
+			target:   6,
+			expected: []int{1, 2},
 		},
-    {
-			desc:          "nums = [3,3], target = 6",
-			nums: [3,3],
-      target: 6,
-			expected:      [0,1],
+		{
+			desc:     "nums = [3,3], target = 6",
+			nums:     []int{3, 3},
+			target:   6,
+			expected: []int{0, 1},
 		},
 	}
 	for _, tt := range tests {
