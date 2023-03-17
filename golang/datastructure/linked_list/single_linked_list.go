19a69e710491f9c88f0047e2aa1e2e11599d707d
chenkun <kun.chen@yintech.cn>
2023-03-17 10:55:28 +0800

    Feat: 单链表

--- golang/datastructure/linked_list/single_linked_list.go
+++ golang/datastructure/linked_list/single_linked_list.go
@@ -1,5 +1,7 @@
 package linked_list
 
+import "fmt"
+
 /*
 单链表基本操作
 author: chenuvi
@@ -19,12 +21,12 @@ func NewListNode(v interface{}) *ListNode {
 	return &ListNode{nil, v}
 }
 
-func GetNext(this *ListNode) *ListNode {
-	return this.next
+func (l *ListNode) GetNext() *ListNode {
+	return l.next
 }
 
-func GetValue(this *ListNode) interface{} {
-	return this.value
+func (l *ListNode) GetValue() interface{} {
+	return l.value
 }
 
 func NewLinkedList() *LinkedList {
@@ -32,36 +34,102 @@ func NewLinkedList() *LinkedList {
 }
 
 // InsertAfter 在某个节点后插入节点
-func InsertAfter() {
-
+func (l *LinkedList) InsertAfter(node *ListNode, v interface{}) bool {
+	if node == nil {
+		return false
+	}
+	newNode := NewListNode(v)
+	oldNext := node.next
+	node.next = newNode
+	newNode.next = oldNext
+	l.length++
+	return true
 }
 
-// InsertBefore 在某个节点后插入节点
-func InsertBefore() {
-
+// InsertBefore 在某个节点前插入节点
+func (l *LinkedList) InsertBefore(node *ListNode, v interface{}) bool {
+	if nil == node || node == l.head {
+		return false
+	}
+	cur := l.head.next
+	pre := l.head
+	if nil == cur {
+		return false
+	}
+	for nil != cur {
+		if cur == node {
+			break
+		}
+		pre = cur
+		cur = cur.next
+	}
+	newNode := NewListNode(v)
+	pre.next = newNode
+	newNode.next = cur
+	l.length++
+	return true
 }
 
 // InsertToHead 在链表头部插入节点
-func InsertToHead() {
-
+func (l *LinkedList) InsertToHead(v interface{}) bool {
+	return l.InsertBefore(l.head, v)
 }
 
 // InsertToTail 在链表尾部部插入节点
-func InsertToTail() {
-
+func (l *LinkedList) InsertToTail(v interface{}) bool {
+	cur := l.head
+	for cur.next != nil {
+		cur = cur.next
+	}
+	return l.InsertAfter(cur, v)
 }
 
 // FindByIndex 通过索引查找节点
-func FindByIndex() {
-
+func (l *LinkedList) FindByIndex(index uint) *ListNode {
+	if index >= l.length {
+		return nil
+	}
+	var i uint = 0
+	cur := l.head.next
+	for ; i < index; i++ {
+		cur = cur.next
+	}
+	return cur
 }
 
 // DeleteNode 删除传入的节点
-func DeleteNode() {
-
+func (l *LinkedList) DeleteNode(node *ListNode) bool {
+	if node == nil {
+		return false
+	}
+	cur := l.head.next
+	pre := l.head
+	for cur != nil {
+		if cur == node {
+			break
+		}
+		pre = cur
+		cur = cur.next
+	}
+	if cur == nil {
+		return false
+	}
+	pre.next = cur.next
+	node = nil
+	l.length--
+	return true
 }
 
 // Print 打印链表
-func Print() {
-
+func (l *LinkedList) Print() {
+	cur := l.head
+	format := ""
+	for cur != nil {
+		format += fmt.Sprintf("%+v", cur.GetValue())
+		cur = cur.next
+		if nil != cur {
+			format += "->"
+		}
+	}
+	fmt.Println(format)
 }
